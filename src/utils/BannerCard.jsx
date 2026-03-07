02f8ba0c2e6641bf2e4b10cf3356d3c548ed6a77
Lê Huy Sĩ <huysi@Mac-mini-cua-Le.local>
2026-03-07 21:00:53 +0700

    “update”

--- src/utils/BannerCard.jsx
+++ src/utils/BannerCard.jsx
@@ -4,23 +4,14 @@ export default function BannerCard({ image, onClick }) {
   return (
     <div
       onClick={onClick}
-      className="
-        w-full
-        cursor-pointer
-        opacity-100
-        h-[78px]
-        lg:h-[210px]
-        lg:rounded-[12px]
-        overflow-hidden
-      "
-    >
+      className="w-full h-[80px] max-h-[80px] lg:rounded-[12px] lg:h-auto lg:max-h-none cursor-pointer overflow-hidden"    >
       <div className="w-full h-full p-0 lg:p-0">
         <img
           src={image}
           alt="banner"
           loading="lazy"
           decoding="async"
-          className="w-full h-full object-cover opacity-100 rotate-0"
+          className="block w-full h-full object-cover lg:h-auto lg:object-contain"
         />
       </div>
     </div>
